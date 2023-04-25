58bf91d343a05ea5e0288f049de6cc495a494860
Barncastle <spearman.tom@gmail.com>
2023-04-25 18:46:10 +0100

    Add bit consumer tests

---
+++ BitsKit.Tests/IO_ReaderTests.cs
@@ -0,0 +1,191 @@
+﻿using System.IO;
+using BitsKit.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BitsKit.Tests;
+
+[TestClass]
+public class IO_ReaderTests
+{
+    private readonly byte[] Data = new byte[]
+    {
+        0xCD, 0x0A, 162, 245, 92, 71, 202, 103, 218, 72
+    };
+
+    private readonly int[] BitCounts = new int[]
+    {
+        7, 10, 43, 0, 13, 6
+    };
+
+    [TestMethod]
+    public void LSBSequentialMatchTest()
+    {
+        using MemoryStream ms = new(Data);
+
+        BitReader bitReader = new(Data);
+        MemoryBitReader memoryBitReader = new(Data);
+        BitStreamReader bitStreamReader = new(ms);
+
+        int bitOffset = 0;
+        foreach (int bitCount in BitCounts)
+        {
+            ulong expected = Helpers.ReadBitsLSB(Data, bitOffset, bitCount);
+
+            ulong readerValue = bitReader.ReadUInt64LSB(bitCount);
+            ulong memoryValue = memoryBitReader.ReadUInt64LSB(bitCount);
+            ulong streamValue = bitStreamReader.ReadUInt64LSB(bitCount);
+
+            Assert.AreEqual(expected, readerValue, "BitReader");
+            Assert.AreEqual(expected, memoryValue, "MemoryBitReader");
+            Assert.AreEqual(expected, streamValue, "BitStreamReader");
+
+            bitOffset += bitCount;
+
+            Assert.AreEqual(bitOffset, bitReader.Position, "BitReader.Position");
+            Assert.AreEqual(bitOffset, memoryBitReader.Position, "MemoryBitReader.Position");
+            Assert.AreEqual(bitOffset, bitStreamReader.Position, "BitStreamReader.Position");
+        }
+    }
+
+    [TestMethod]
+    public void MSBSequentialMatchTest()
+    {
+        using MemoryStream ms = new(Data);
+
+        BitReader bitReader = new(Data);
+        MemoryBitReader memoryBitReader = new(Data);
+        BitStreamReader bitStreamReader = new(ms);
+
+        int bitOffset = 0;
+        foreach (int bitCount in BitCounts)
+        {
+            ulong expected = Helpers.ReadBitsMSB(Data, bitOffset, bitCount);
+
+            ulong readerValue = bitReader.ReadUInt64MSB(bitCount);
+            ulong memoryValue = memoryBitReader.ReadUInt64MSB(bitCount);
+            ulong streamValue = bitStreamReader.ReadUInt64MSB(bitCount);
+
+            Assert.AreEqual(expected, readerValue, "BitReader");
+            Assert.AreEqual(expected, memoryValue, "MemoryBitReader");
+            Assert.AreEqual(expected, streamValue, "BitStreamReader");
+
+            bitOffset += bitCount;
+
+            Assert.AreEqual(bitOffset, bitReader.Position, "BitReader.Position");
+            Assert.AreEqual(bitOffset, memoryBitReader.Position, "MemoryBitReader.Position");
+            Assert.AreEqual(bitOffset, bitStreamReader.Position, "BitStreamReader.Position");
+        }
+    }
+
+    [TestMethod]
+    public void LSBNonSequentialMatchTest()
+    {
+        using MemoryStream ms = new(Data);
+
+        BitReader bitReader = new(Data);
+        MemoryBitReader memoryBitReader = new(Data);
+        BitStreamReader bitStreamReader = new(ms);
+
+        int bitOffset = 80;
+        foreach (int bitCount in BitCounts)
+        {
+            bitOffset -= bitCount;
+
+            ulong expected = Helpers.ReadBitsLSB(Data, bitOffset, bitCount);
+
+            bitReader.Position = bitOffset;
+            memoryBitReader.Position = bitOffset;
+            bitStreamReader.Position = bitOffset;
+
+            ulong readerValue = bitReader.ReadUInt64LSB(bitCount);
+            ulong memoryValue = memoryBitReader.ReadUInt64LSB(bitCount);
+            ulong streamValue = bitStreamReader.ReadUInt64LSB(bitCount);
+
+            Assert.AreEqual(expected, readerValue, "BitReader");
+            Assert.AreEqual(expected, memoryValue, "MemoryBitReader");
+            Assert.AreEqual(expected, streamValue, "BitStreamReader");
+        }
+    }
+
+    [TestMethod]
+    public void MSBNonSequentialMatchTest()
+    {
+        using MemoryStream ms = new(Data);
+
+        BitReader bitReader = new(Data);
+        MemoryBitReader memoryBitReader = new(Data);
+        BitStreamReader bitStreamReader = new(ms);
+
+        int bitOffset = 80;
+        foreach (int bitCount in BitCounts)
+        {
+            bitOffset -= bitCount;
+
+            ulong expected = Helpers.ReadBitsMSB(Data, bitOffset, bitCount);
+
+            bitReader.Position = bitOffset;
+            memoryBitReader.Position = bitOffset;
+            bitStreamReader.Position = bitOffset;
+
+            ulong readerValue = bitReader.ReadUInt64MSB(bitCount);
+            ulong memoryValue = memoryBitReader.ReadUInt64MSB(bitCount);
+            ulong streamValue = bitStreamReader.ReadUInt64MSB(bitCount);
+
+            Assert.AreEqual(expected, readerValue, "BitReader");
+            Assert.AreEqual(expected, memoryValue, "MemoryBitReader");
+            Assert.AreEqual(expected, streamValue, "BitStreamReader");
+        }
+    }
+
+    [TestMethod]
+    [DataRow(0)]
+    [DataRow(1)]
+    public void LSBBitMatchTest(int bitOffset)
+    {
+        using MemoryStream ms = new(Data);
+
+        BitReader bitReader = new(Data);
+        MemoryBitReader memoryBitReader = new(Data);
+        BitStreamReader bitStreamReader = new(ms);
+
+        bool expected = Helpers.ReadBitsLSB(Data, bitOffset, 1) == 1;
+
+        bitReader.Position = bitOffset;
+        memoryBitReader.Position = bitOffset;
+        bitStreamReader.Position = bitOffset;
+
+        bool readerValue = bitReader.ReadBitLSB();
+        bool memoryValue = memoryBitReader.ReadBitLSB();
+        bool streamValue = bitStreamReader.ReadBitLSB();
+
+        Assert.AreEqual(expected, readerValue, "BitReader");
+        Assert.AreEqual(expected, memoryValue, "MemoryBitReader");
+        Assert.AreEqual(expected, streamValue, "BitStreamReader");
+    }
+
+    [TestMethod]
+    [DataRow(0)]
+    [DataRow(1)]
+    public void MSBBitMatchTest(int bitOffset)
+    {
+        using MemoryStream ms = new(Data);
+
+        BitReader bitReader = new(Data);
+        MemoryBitReader memoryBitReader = new(Data);
+        BitStreamReader bitStreamReader = new(ms);
+
+        bool expected = Helpers.ReadBitsMSB(Data, bitOffset, 1) == 1;
+
+        bitReader.Position = bitOffset;
+        memoryBitReader.Position = bitOffset;
+        bitStreamReader.Position = bitOffset;
+
+        bool readerValue = bitReader.ReadBitMSB();
+        bool memoryValue = memoryBitReader.ReadBitMSB();
+        bool streamValue = bitStreamReader.ReadBitMSB();
+
+        Assert.AreEqual(expected, readerValue, "BitReader");
+        Assert.AreEqual(expected, memoryValue, "MemoryBitReader");
+        Assert.AreEqual(expected, streamValue, "BitStreamReader");
+    }
+}
---
+++ BitsKit.Tests/IO_WriterTests.cs
@@ -0,0 +1,260 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using BitsKit.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json.Linq;
+
+namespace BitsKit.Tests;
+
+[TestClass]
+public class IO_WriterTests
+{
+    private readonly byte[] Data = new byte[]
+    {
+        0xCD, 0x0A, 162, 245, 92, 71, 202, 103, 218, 72
+    };
+
+    private readonly int[] BitCounts = new int[]
+    {
+        7, 10, 43, 0, 13, 6
+    };
+
+    [TestMethod]
+    public void LSBSequentialMatchTest()
+    {
+        byte[] expected = Data[..];
+        byte[] data1 = Data[..];
+        byte[] data2 = Data[..];
+        byte[] data3 = Data[..];
+        using MemoryStream ms = new(data3);
+
+        BitWriter bitWriter = new(data1);
+        MemoryBitWriter memoryBitWriter = new(data2);
+        BitStreamWriter bitStreamWriter = new(ms);
+
+        ulong value = Unsafe.ReadUnaligned<ulong>(ref Data[2]);
+
+        int bitOffset = 0;
+        foreach (int bitCount in BitCounts)
+        {
+            Helpers.WriteBitsLSB(expected, bitOffset, value, bitCount);
+
+            bitWriter.WriteUInt64LSB(value, bitCount);
+            memoryBitWriter.WriteUInt64LSB(value, bitCount);
+            bitStreamWriter.WriteUInt64LSB(value, bitCount);
+
+            bitOffset += bitCount;
+
+            // BitStreamWriter buffers partially written bytes
+            // so only comparing the number of whole bytes written
+            int byteCount = bitOffset >> 3;
+
+            CollectionAssert.AreEqual(expected[..byteCount], data1[..byteCount], "BitWriter");
+            CollectionAssert.AreEqual(expected[..byteCount], data2[..byteCount], "MemoryBitWriter");
+            CollectionAssert.AreEqual(expected[..byteCount], data3[..byteCount], "BitStreamWriter");                       
+
+            Assert.AreEqual(bitOffset, bitWriter.Position, "BitWriter.Position");
+            Assert.AreEqual(bitOffset, memoryBitWriter.Position, "MemoryBitWriter.Position");
+            Assert.AreEqual(bitOffset, bitStreamWriter.Position, "BitStreamWriter.Position");
+        }
+
+        // write remaining bits to stream
+        bitStreamWriter.Flush();
+
+        CollectionAssert.AreEqual(expected, data1, "BitWriter");
+        CollectionAssert.AreEqual(expected, data2, "MemoryBitWriter");
+        CollectionAssert.AreEqual(expected, data3, "BitStreamWriter");
+    }
+
+    [TestMethod]
+    public void MSBSequentialMatchTest()
+    {
+        byte[] expected = Data[..];
+        byte[] data1 = Data[..];
+        byte[] data2 = Data[..];
+        byte[] data3 = Data[..];
+        using MemoryStream ms = new(data3);
+
+        BitWriter bitWriter = new(data1);
+        MemoryBitWriter memoryBitWriter = new(data2);
+        BitStreamWriter bitStreamWriter = new(ms);
+
+        ulong value = Unsafe.ReadUnaligned<ulong>(ref Data[2]);
+
+        int bitOffset = 0;
+        foreach (int bitCount in BitCounts)
+        {
+            Helpers.WriteBitsLSB(expected, bitOffset, value, bitCount);
+
+            bitWriter.WriteUInt64LSB(value, bitCount);
+            memoryBitWriter.WriteUInt64LSB(value, bitCount);
+            bitStreamWriter.WriteUInt64LSB(value, bitCount);
+
+            bitOffset += bitCount;
+
+            // BitStreamWriter buffers partially written bytes
+            // so only comparing the number of whole bytes written
+            int byteCount = bitOffset >> 3;
+
+            CollectionAssert.AreEqual(expected[..byteCount], data1[..byteCount], "BitWriter");
+            CollectionAssert.AreEqual(expected[..byteCount], data2[..byteCount], "MemoryBitWriter");
+            CollectionAssert.AreEqual(expected[..byteCount], data3[..byteCount], "BitStreamWriter");
+
+            Assert.AreEqual(bitOffset, bitWriter.Position, "BitWriter.Position");
+            Assert.AreEqual(bitOffset, memoryBitWriter.Position, "MemoryBitWriter.Position");
+            Assert.AreEqual(bitOffset, bitStreamWriter.Position, "BitStreamWriter.Position");
+        }
+
+        // write remaining bits to stream
+        bitStreamWriter.Flush();
+
+        CollectionAssert.AreEqual(expected, data1, "BitWriter");
+        CollectionAssert.AreEqual(expected, data2, "MemoryBitWriter");
+        CollectionAssert.AreEqual(expected, data3, "BitStreamWriter");
+    }
+
+    [TestMethod]
+    public void LSBNonSequentialMatchTest()
+    {
+        byte[] expected = Data[..];
+        byte[] data1 = Data[..];
+        byte[] data2 = Data[..];
+        byte[] data3 = Data[..];
+        using MemoryStream ms = new(data3);
+
+        BitWriter bitWriter = new(data1);
+        MemoryBitWriter memoryBitWriter = new(data2);
+        BitStreamWriter bitStreamWriter = new(ms);
+
+        ulong value = Unsafe.ReadUnaligned<ulong>(ref Data[2]);
+
+        int bitOffset = 80;
+        foreach (int bitCount in BitCounts)
+        {
+            bitOffset -= bitCount;
+
+            Helpers.WriteBitsLSB(expected, bitOffset, value, bitCount);
+
+            bitWriter.Position = bitOffset;
+            memoryBitWriter.Position = bitOffset;
+            bitStreamWriter.Position = bitOffset;
+
+            bitWriter.WriteUInt64LSB(value, bitCount);
+            memoryBitWriter.WriteUInt64LSB(value, bitCount);
+            bitStreamWriter.WriteUInt64LSB(value, bitCount);
+
+            bitStreamWriter.Flush();
+
+            CollectionAssert.AreEqual(expected, data1, "BitWriter");
+            CollectionAssert.AreEqual(expected, data2, "MemoryBitWriter");
+            CollectionAssert.AreEqual(expected, data3, "BitStreamWriter");
+        }
+    }
+
+    [TestMethod]
+    public void MSBNonSequentialMatchTest()
+    {
+        byte[] expected = Data[..];
+        byte[] data1 = Data[..];
+        byte[] data2 = Data[..];
+        byte[] data3 = Data[..];
+        using MemoryStream ms = new(data3);
+
+        BitWriter bitWriter = new(data1);
+        MemoryBitWriter memoryBitWriter = new(data2);
+        BitStreamWriter bitStreamWriter = new(ms);
+
+        ulong value = Unsafe.ReadUnaligned<ulong>(ref Data[2]);
+
+        int bitOffset = 80;
+        foreach (int bitCount in BitCounts)
+        {
+            bitOffset -= bitCount;
+
+            Helpers.WriteBitsMSB(expected, bitOffset, value, bitCount);
+
+            bitWriter.Position = bitOffset;
+            memoryBitWriter.Position = bitOffset;
+            bitStreamWriter.Position = bitOffset;
+
+            bitWriter.WriteUInt64MSB(value, bitCount);
+            memoryBitWriter.WriteUInt64MSB(value, bitCount);
+            bitStreamWriter.WriteUInt64MSB(value, bitCount);
+
+            bitStreamWriter.Flush();
+
+            CollectionAssert.AreEqual(expected, data1, "BitWriter");
+            CollectionAssert.AreEqual(expected, data2, "MemoryBitWriter");
+            CollectionAssert.AreEqual(expected, data3, "BitStreamWriter");
+        }
+    }
+
+    [TestMethod]
+    [DataRow(0)]
+    [DataRow(1)]
+    public void LSBBitMatchTest(int bitOffset)
+    {
+        byte[] expected = Data[..];
+        byte[] data1 = Data[..];
+        byte[] data2 = Data[..];
+        byte[] data3 = Data[..];
+        using MemoryStream ms = new(data3);
+
+        BitWriter bitWriter = new(data1);
+        MemoryBitWriter memoryBitWriter = new(data2);
+        BitStreamWriter bitStreamWriter = new(ms);
+
+        Helpers.WriteBitsLSB(expected, bitOffset, 1, 1);
+
+        bitWriter.Position = bitOffset;
+        memoryBitWriter.Position = bitOffset;
+        bitStreamWriter.Position = bitOffset;
+
+        bitWriter.WriteBitLSB(true);
+        memoryBitWriter.WriteBitLSB(true);
+        bitStreamWriter.WriteBitLSB(true);
+
+        bitStreamWriter.Flush();
+
+        CollectionAssert.AreEqual(expected, data1, "BitWriter");
+        CollectionAssert.AreEqual(expected, data2, "MemoryBitWriter");
+        CollectionAssert.AreEqual(expected, data3, "BitStreamWriter");
+    }
+
+    [TestMethod]
+    [DataRow(0)]
+    [DataRow(1)]
+    public void MSBBitMatchTest(int bitOffset)
+    {
+        byte[] expected = Data[..];
+        byte[] data1 = Data[..];
+        byte[] data2 = Data[..];
+        byte[] data3 = Data[..];
+        using MemoryStream ms = new(data3);
+
+        BitWriter bitWriter = new(data1);
+        MemoryBitWriter memoryBitWriter = new(data2);
+        BitStreamWriter bitStreamWriter = new(ms);
+
+        Helpers.WriteBitsMSB(expected, bitOffset, 1, 1);
+
+        bitWriter.Position = bitOffset;
+        memoryBitWriter.Position = bitOffset;
+        bitStreamWriter.Position = bitOffset;
+
+        bitWriter.WriteBitLSB(true);
+        memoryBitWriter.WriteBitMSB(true);
+        bitStreamWriter.WriteBitMSB(true);
+
+        bitStreamWriter.Flush();
+
+        CollectionAssert.AreEqual(expected, data1, "BitWriter");
+        CollectionAssert.AreEqual(expected, data2, "MemoryBitWriter");
+        CollectionAssert.AreEqual(expected, data3, "BitStreamWriter");
+    }
+}
