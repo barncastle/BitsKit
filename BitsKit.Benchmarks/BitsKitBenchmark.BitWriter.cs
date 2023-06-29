d15b20afb425fe840e4d477de22074e389ab90de
Barncastle <spearman.tom@gmail.com>
2023-06-29 17:41:55 +0100

    add BitWriter benchmarks and cleanup dev classes

---
+++ BitsKit.Benchmarks/BitsKitBenchmark.BitWriter.cs
@@ -0,0 +1,143 @@
+﻿using BenchmarkDotNet.Attributes;
+using BitsKit.IO;
+
+namespace BitsKit.Benchmarks;
+
+public partial class BitsKitBenchmark
+{
+    [Benchmark]
+    [BenchmarkCategory("BitWriter", "UInt1")]
+    public int BitWriterBit()
+    {
+        var maxNumIterations = BufferSize;
+        var writer = new BitWriter(WriteBuffer);
+
+        for (var numIterations = 0; numIterations < maxNumIterations; numIterations++)
+        {
+            writer.WriteBitMSB(false);
+            writer.WriteBitMSB(true);
+            writer.WriteBitMSB(false);
+            writer.WriteBitMSB(true);
+            writer.WriteBitMSB(false);
+            writer.WriteBitMSB(true);
+            writer.WriteBitMSB(false);
+            writer.WriteBitMSB(true);
+        }
+
+        return 0;
+    }
+    [Benchmark]
+    [BenchmarkCategory("BitWriter", "UInt4")]
+    public int BitWriterUInt04()
+    {
+        const int bitCount = 4;
+        int maxNumIterations = BufferSize / bitCount;
+        var writer = new BitWriter(WriteBuffer);
+
+        for (var numIterations = 0; numIterations < maxNumIterations; numIterations++)
+        {
+            writer.WriteUInt8MSB((byte)numIterations, 3);
+            writer.WriteUInt8MSB((byte)numIterations, 4);
+            writer.WriteUInt8MSB((byte)numIterations, 3);
+            writer.WriteUInt8MSB((byte)numIterations, 4);
+            writer.WriteUInt8MSB((byte)numIterations, 3);
+            writer.WriteUInt8MSB((byte)numIterations, 4);
+            writer.WriteUInt8MSB((byte)numIterations, 3);
+            writer.WriteUInt8MSB((byte)numIterations, 4);
+        }
+
+        return 0;
+    }
+
+    [Benchmark]
+    [BenchmarkCategory("BitWriter", "UInt8")]
+    public int BitWriterUInt08()
+    {
+        const int bitCount = 8;
+        int maxNumIterations = BufferSize / bitCount;
+        var writer = new BitWriter(WriteBuffer);
+
+        for (var numIterations = 0; numIterations < maxNumIterations; numIterations++)
+        {
+            writer.WriteUInt8MSB((byte)numIterations, 1);
+            writer.WriteUInt8MSB((byte)numIterations, 2);
+            writer.WriteUInt8MSB((byte)numIterations, 3);
+            writer.WriteUInt8MSB((byte)numIterations, 4);
+            writer.WriteUInt8MSB((byte)numIterations, 5);
+            writer.WriteUInt8MSB((byte)numIterations, 6);
+            writer.WriteUInt8MSB((byte)numIterations, 7);
+            writer.WriteUInt8MSB((byte)numIterations, 8);
+        }
+
+        return 0;
+    }
+
+    [Benchmark]
+    [BenchmarkCategory("BitWriter", "UInt16")]
+    public int BitWriterUInt16()
+    {
+        const int bitCount = 16;
+        int maxNumIterations = BufferSize / bitCount;
+        var writer = new BitWriter(WriteBuffer);
+
+        for (var numIterations = 0; numIterations < maxNumIterations; numIterations++)
+        {
+            writer.WriteUInt16MSB((ushort)numIterations, 15);
+            writer.WriteUInt16MSB((ushort)numIterations, 16);
+            writer.WriteUInt16MSB((ushort)numIterations, 15);
+            writer.WriteUInt16MSB((ushort)numIterations, 16);
+            writer.WriteUInt16MSB((ushort)numIterations, 15);
+            writer.WriteUInt16MSB((ushort)numIterations, 16);
+            writer.WriteUInt16MSB((ushort)numIterations, 15);
+            writer.WriteUInt16MSB((ushort)numIterations, 16);
+        }
+
+        return 0;
+    }
+
+    [Benchmark]
+    [BenchmarkCategory("BitWriter", "UInt32")]
+    public int BitWriterUInt32()
+    {
+        const int bitCount = 32;
+        int maxNumIterations = BufferSize / bitCount;
+        var writer = new BitWriter(WriteBuffer);
+
+        for (var numIterations = 0; numIterations < maxNumIterations; numIterations++)
+        {
+            writer.WriteUInt32MSB((uint)numIterations, 31);
+            writer.WriteUInt32MSB((uint)numIterations, 32);
+            writer.WriteUInt32MSB((uint)numIterations, 31);
+            writer.WriteUInt32MSB((uint)numIterations, 32);
+            writer.WriteUInt32MSB((uint)numIterations, 31);
+            writer.WriteUInt32MSB((uint)numIterations, 32);
+            writer.WriteUInt32MSB((uint)numIterations, 31);
+            writer.WriteUInt32MSB((uint)numIterations, 32);
+        }
+
+        return 0;
+    }
+
+    [Benchmark]
+    [BenchmarkCategory("BitWriter", "UInt64")]
+    public int BitWriterUInt64()
+    {
+        const int bitCount = 64;
+        int maxNumIterations = BufferSize / bitCount;
+        var writer = new BitWriter(WriteBuffer);
+
+        for (var numIterations = 0; numIterations < maxNumIterations; numIterations++)
+        {
+            writer.WriteUInt64MSB((ulong)numIterations, 63);
+            writer.WriteUInt64MSB((ulong)numIterations, 64);
+            writer.WriteUInt64MSB((ulong)numIterations, 63);
+            writer.WriteUInt64MSB((ulong)numIterations, 64);
+            writer.WriteUInt64MSB((ulong)numIterations, 63);
+            writer.WriteUInt64MSB((ulong)numIterations, 64);
+            writer.WriteUInt64MSB((ulong)numIterations, 63);
+            writer.WriteUInt64MSB((ulong)numIterations, 64);
+        }
+
+        return 0;
+    }
+}
--- BitsKit.Benchmarks/Program.cs
+++ BitsKit.Benchmarks/Program.cs
@@ -7,9 +7,20 @@ public static class Program
 {
     static void Main()
     {
-        BenchmarkRunner.Run<DevBenchmark>(new MultipleRuntimesConfig(MultipleRuntimesFlags.all));
-        //BenchmarkRunner.Run<BitsKitBenchmark>(new MultipleRuntimesConfig(MultipleRuntimesFlags.all, "BitPrimitivesWriteMSB", "UInt32"));
-        //BenchmarkRunner.Run<BitsKitBenchmark>(new MultipleRuntimesConfig(MultipleRuntimesFlags.all, "Loop", "BitPrimitivesWriteMSB", "UInt64"));
+        BenchmarkRunner.Run<BitsKitBenchmark>(new MultipleRuntimesConfig(MultipleRuntimesFlags.all, "BitPrimitivesReadLSB"));
+        BenchmarkRunner.Run<BitsKitBenchmark>(new MultipleRuntimesConfig(MultipleRuntimesFlags.all, "BitPrimitivesReadMSB"));
+
+        BenchmarkRunner.Run<BitsKitBenchmark>(new MultipleRuntimesConfig(MultipleRuntimesFlags.all, "BitPrimitivesWriteLSB"));
+        BenchmarkRunner.Run<BitsKitBenchmark>(new MultipleRuntimesConfig(MultipleRuntimesFlags.all, "BitPrimitivesWriteMSB"));
+
+        BenchmarkRunner.Run<BitsKitBenchmark>(new MultipleRuntimesConfig(MultipleRuntimesFlags.all, "BitReader"));
+        BenchmarkRunner.Run<BitsKitBenchmark>(new MultipleRuntimesConfig(MultipleRuntimesFlags.all, "MemoryBitReader"));
+        BenchmarkRunner.Run<BitsKitBenchmark>(new MultipleRuntimesConfig(MultipleRuntimesFlags.all, "BitStreamReader"));
+
+        BenchmarkRunner.Run<BitsKitBenchmark>(new MultipleRuntimesConfig(MultipleRuntimesFlags.all, "BitWriter"));
+        BenchmarkRunner.Run<BitsKitBenchmark>(new MultipleRuntimesConfig(MultipleRuntimesFlags.all, "MemoryBitWriter"));
+        BenchmarkRunner.Run<BitsKitBenchmark>(new MultipleRuntimesConfig(MultipleRuntimesFlags.all, "BitStreamWriter"));
+
         Console.ReadLine();
     }
 }
